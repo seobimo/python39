4ab1d7ff70cf428a5d906936ad50823d28ce665a
seobimo <gstar3078@naver.com>
2022-10-11 18:19:00 +0900

    변수, 연산자, 예제

---
+++ 04example.py
@@ -0,0 +1,64 @@
+#1
+#프로그래밍 언어 실습시 글꼴은
+#고정폭 글꼴을 사용할 것을 추천
+print('*   *    **    ****    ****    *   *           /////               ')
+print('*   *   *  *   *   *   *   *   *   *          | o o |              ')
+print('*****  *    *  ****    ****     * *          (|  ^  |)             ')
+print('*   *  ******  *   *   *   *     *            | [_] |              ')
+print('*   *  *    *  *    *  *    *    *             _____               ')
+
+
+print('            +---+                                                  ')
+print('            |   |                                                  ')
+print('        +---+---+                                                  ')
+print('        |   |   |                                                  ')
+print('    +---+---+---+           /\\ __/\\         -----                ')
+print('    |   |   |   |          (   ; ;   )     /  Hello  \             ')
+print('+---+---+---+---+          (    -    )    <   Junior  |            ')
+print('|   |   |   |   |           |   |   |      \  Coder!  /            ')
+print('+---+---+---+---+          (    |    )        -----                ')
+
+
+#3
+rate1 = 1
+#1stPlayer = 2       #변수명이 숫자로 시작
+#myprogam.java = 3   #.때문에 불가
+long = 4
+timeLimit = 5
+numberOfWindow = 6
+
+
+#4
+x, y, z = 1, 2, 3
+s0, v0, t, g = 4, 5, 6, 9.8
+print(3 * x, 3 * x + y, x + y / 7)
+print(s0 + v0*t + (1/2) * g * t**2)
+
+
+#6
+x = 2.5
+y = 1.5
+m = 18
+n = 4
+print(x + n * y - (x + n) * y)
+print(m / n + m%n)
+print(5 * x - n / 5)
+print(1  -(1 - (1 -( - 1( - (1 - n)))))
+
+
+#7
+#숫자 연산자들은 bool 함수를 써야함
+print(3 + 4.5 * 2 + 27 / 8)
+print(True or False and 3 < 4 or not(5 == 7))
+print(True or (3 < 5 and 6 >= 2))
+print  ( not True > 'A')  #'A' => bool('A')  #not (True > True)
+print(7 % 4 + 3 - 2 / 6 * 'Z') #'Z' => bool('Z')
+print('D' + 1 + 'M' % 2 / 3)  #'D','M' => bool('D') bool('M')
+print(5.0 / 3 + 3 / 3 )
+print(53 % 21 < 45 / 18)
+print((4 < 6) or True and False or False and (2 > 3))
+print(7 - (3 + 8 * 6 + 3) - (2 + 5 * 2))
+
+#9
+
+#10
